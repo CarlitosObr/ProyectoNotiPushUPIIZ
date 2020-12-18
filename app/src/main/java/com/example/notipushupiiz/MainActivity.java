c297ffe86b8c5fdf85f2de090ff43515abcfcaad
JairoHernandez <jairohernandez616mail.com>
2020-12-17 21:04:46 -0600

    --holaaaa

--- app/src/main/java/com/example/notipushupiiz/MainActivity.java
+++ app/src/main/java/com/example/notipushupiiz/MainActivity.java
@@ -5,8 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private ExpandableListView expLV;
+    private ExpLVAdapter adapter;
+    private ArrayList<String> listCategorias;
+    private Map<String, ArrayList<String >> mapChild;
     Button b;
     EditText sobres;
     EditText et;
@@ -16,5 +25,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expLV = findViewById(R.id.expLV);
+        listCategorias = new ArrayList<>();
+        mapChild = new HashMap<>();
+
+        cargarDatos();
+
+    }
+    private void cargarDatos(){
+        ArrayList<String> listSeleccionar = new ArrayList<>();
+
+        listCategorias.add("Seleccionar");
+
+        listSeleccionar.add("opcion1");
+        listSeleccionar.add("opcion2");
+        listSeleccionar.add("opcion3");
+        listSeleccionar.add("opcion4");
+
+        mapChild.put(listCategorias.get(0), listSeleccionar);
+
+        adapter = new ExpLVAdapter(this, listCategorias, mapChild);
+        expLV.setAdapter(adapter);
+
+
     }
 }
